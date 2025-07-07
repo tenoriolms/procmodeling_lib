73038c598425b388e1128fab33c8804a665563b4
lhucas <lhucas_tenorio@hotmail.com>
2025-07-07 18:41:19 -0300

    bugs fixed

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='procmodeling',
-    version='0.0.2',
+    version='1.0.0',
     author='tenoriolms',
     description='',
     long_description='',#open('README.md').read(),
